61e9792ec069dd5b58e631cf1a154d079c819208
Greg <gfitzpatrick2@gmail.com>
2021-02-06 10:48:59 -0500

    initial commit

---
+++ env/src/encrypt.js
@@ -0,0 +1,37 @@
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+
+const prompt = inquirer.createPromptModule();
+
+prompt({
+  message: 'Enter Password: ',
+  type: 'password',
+  name: 'key'
+})
+  .then(answer => {
+    fs.readFile(path.join(__dirname, '../.env'), 'utf8', (err, data) => {
+      if (err) {
+        throw err;
+      }
+      const iv = crypto.randomBytes(16);
+      const cipher = crypto.createCipheriv('aes-128-cbc', new Buffer.from(answer.key, 'utf8'), iv);
+      const encrypted = cipher.update(data);
+      const finalBuffer = Buffer.concat([encrypted, cipher.final()]);
+      const encryptedHex = iv.toString('hex') + ':' + finalBuffer.toString('hex');
+
+      fs.writeFile(path.join(__dirname, '../encrypted.env'), encryptedHex, (err, data) => {
+        if (err) {
+          throw err;
+        }
+        console.log('Data successfully encrypted.');
+      });
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  })
+
+
+
